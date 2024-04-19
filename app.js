32c4dfaa47b8e4c1a5ccc35f4034d0852be926e2
iqra22topak22 <161644859+iqra22topak22@users.noreply.github.com>
2024-04-19 21:27:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+console.log("guess a number 1 to 8");
+//now variable create
+let a = Math.floor(Math.random() * 7);
+import inquirer from "inquirer";
+while (true) {
+    let input = await inquirer.prompt({ name: "guess", type: "number", message: "enter your guess number you want between 1 to 8" });
+    //provide code
+    let ans = input.guess;
+    if (a == ans) {
+        console.log("congratulation your guess number is correct");
+        break;
+    }
+    else {
+        console.log("sorry you guess wrong number try again");
+    }
+}
